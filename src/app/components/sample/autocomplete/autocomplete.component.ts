7d85e516d3c2a313352f90ece60fd079215a7d53
Anderson Braz <braz_anderson@msn.com>
2021-02-25 12:16:40 -0300

    Up File(s)

--- src/app/components/sample/autocomplete/autocomplete.component.ts
+++ src/app/components/sample/autocomplete/autocomplete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-autocomplete',
@@ -17,13 +20,26 @@ export class AutocompleteComponent implements OnInit {
     { name: 'Javascript' }
   ];
 
+  myControl = new FormControl();
+  filterOptions: Observable<string[]>;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.filterOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    )
   }
 
   displayFn(subject: { name: any; }){
     return subject ? subject.name : undefined;
+  }
+
+  private _filter(value: string): string[]{
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
 }
