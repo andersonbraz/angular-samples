de393fd1b22732e9f5229746955e1d920aafe8a4
Anderson Braz <braz_anderson@msn.com>
2021-03-06 12:18:34 -0300

    Up File(s)

--- src/app/components/sample/snackbar/snackbar.component.ts
+++ src/app/components/sample/snackbar/snackbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+  styleUrls: ['./snackbar.component.css'],
 })
 export class SnackbarComponent implements OnInit {
+  constructor(private snackbar: MatSnackBar) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openSnackMessage(message: string, action: string): void {
+    let snackBarRef = this.snackbar.open(message, action, {
+      duration: 2000,
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+    });
+
+    snackBarRef.afterDismissed().subscribe(() => {
+      console.log('Here Dismissed!');
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      console.log('Here Action!');
+    });
   }
-
 }
