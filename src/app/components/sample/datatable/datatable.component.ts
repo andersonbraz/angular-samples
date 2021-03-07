b0f7a3aa3176e065c17cc24ed4b7a5b65a944769
Anderson Braz <braz_anderson@msn.com>
2021-03-07 14:56:25 -0300

    Up File(s)

--- src/app/components/sample/datatable/datatable.component.ts
+++ src/app/components/sample/datatable/datatable.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { ElementService } from './element/element.service';
 
 @Component({
@@ -10,29 +10,20 @@ import { ElementService } from './element/element.service';
   styleUrls: ['./datatable.component.css'],
 })
 export class DatatableComponent implements OnInit {
-
-  ELEMENT_DATA: Element[] = [];
-
+  elements: Element[] = [];
   displayedColumns: string[] = ['position', 'name', 'atomic_mass', 'symbol'];
-  dataSource = new MatTableDataSource(this.ELEMENT_DATA);
-
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor(private elementService: ElementService) {}
 
   ngOnInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.elementService.getElements();
+    this.elementService.getElements().subscribe((elements) => {
+      this.elements = elements;
+      console.log(elements);
+    });
   }
 
   logData(row: object): void {
     console.log(row);
-  }
-
-  applyFilter(text: string): void {
-    this.dataSource.filter = text.trim().toLowerCase();
   }
 
 }
--- src/app/components/sample/datatable/element/element.service.ts
+++ src/app/components/sample/datatable/element/element.service.ts
@@ -1,17 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ElementService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private snackBar: MatSnackBar, private httpClient: HttpClient) {}
 
-  getElements(): void {
-    this.httpClient
+  getElements(): Observable<Element[]> {
+    return this.httpClient
       .get<Element[]>('assets/data/periodic-table.json')
-      .subscribe((data) => {
-        console.log(data);
-      });
+      .pipe(
+        map((obj) => obj),
+        catchError((e) => this.errorHandler(e))
+      );
   }
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
+    });
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
+  }
+  
 }
