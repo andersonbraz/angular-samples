6a3aa73378ba0606d4ecdcf41082a8b6f3659724
Anderson Braz <braz_anderson@msn.com>
2021-03-10 01:12:24 -0300

    Up File(s)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuideComponent } from './components/template/guide/guide.component';
+import { TypographyComponent } from './components/sample/typography/typography.component';
+import { IconComponent } from './components/sample/icon/icon.component';
+import { ButtonComponent } from './components/sample/button/button.component';
+import { CardComponent } from './components/sample/card/card.component';
+import { SidenavComponent } from './components/sample/sidenav/sidenav.component';
+import { DatatableComponent } from './components/sample/datatable/datatable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'typography',
+    component: TypographyComponent,
+  },
+  {
+    path: 'buttons',
+    component: ButtonComponent,
+  },
+  {
+    path: 'icons',
+    component: IconComponent,
+  },
+  {
+    path: 'cards',
+    component: CardComponent,
+  },
+  {
+    path: 'sidenav',
+    component: SidenavComponent,
+  },
+  {
+    path: 'data-table',
+    component: DatatableComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/template/guide/guide.component.ts
+++ src/app/components/template/guide/guide.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guide.component.css'],
 })
 export class GuideComponent implements OnInit {
-
+  list = [
+    { icon: 'home', text: 'Home', link: '/' },
+    { icon: 'design_services', text: 'Typography', link: '/typography' },
+    { icon: 'filter_none', text: 'Button', link: '/buttons' },
+    { icon: 'filter_center_focus', text: 'Icons', link: '/icons' },
+    { icon: 'favorite', text: 'Cards', link: '/cards' },
+    { icon: 'list', text: 'Sidenav', link: '/sidenav' },
+    { icon: 'table_view', text: 'Datatable', link: '/data-table' },
+    { icon: '', text: '', link: '' },
+    { icon: '', text: '', link: '' },
+    { icon: '', text: '', link: '' },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
