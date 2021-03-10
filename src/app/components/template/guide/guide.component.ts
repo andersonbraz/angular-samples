d7606c1a7bb0b22969804b4bfb73ec6daf2df228
Anderson Braz <braz_anderson@msn.com>
2021-03-09 23:49:43 -0300

    Up File(s)

--- src/app/components/template/guide/guide.component.ts
+++ src/app/components/template/guide/guide.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guide.component.css'],
 })
 export class GuideComponent implements OnInit {
-  showFiller = true;
 
   constructor() {}
 
